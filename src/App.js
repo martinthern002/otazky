d38662b3f95d6b52eadb5edd6ee596eda2780ecf
mt648sh <thernm2000@gmail.com>
2024-06-15 19:21:57 +0200

    Arch

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import React, { useState, useEffect } from "react";
 import p_h from "./questions/physics_questions.json";
 import q from "./questions/questions.json";
 import t_q from "./questions/tech_questions.json";
+import q_2 from "./questions/questions2.json";
 
 function App() {
   
   const [current_1, setCurrent_1] = useState(null);
-    const [current_2, setCurrent_2] = useState(null);
-    const [current_3, setCurrent_3] = useState(null);
+  const [current_2, setCurrent_2] = useState(null);
+  const [current_3, setCurrent_3] = useState(null);
+  const [current_4, setCurrent_4] = useState(null);
 
     const [questions_1, setQuestions_1] = useState([]);
     const [questions_2, setQuestions_2] = useState([]);
     const [questions_3, setQuestions_3] = useState([]);
+    const [questions_4, setQuestions_4] = useState([]);
 
     useEffect(() => {
         // Načítaj otázky pri montovaní komponentu
         setQuestions_1(q);
         setQuestions_2(p_h);
         setQuestions_3(t_q);
+        setQuestions_4(q_2);
     }, []);
 
     const randomNumbers = (filename) => {
@@ -40,13 +44,19 @@ function App() {
                 setCurrent_3(questions_3[randomIndex]);
             }
         }
+        else if (filename === 4) {
+          if (questions_4.length > 0) {
+              const randomIndex = Math.floor(Math.random() * questions_4.length);
+              setCurrent_4(questions_4[randomIndex]);
+          }
+      }
 
     }
 
     return (
         <div style={{width: '80%', margin: 'auto'}}>
             <h1>Otázka pre Bc - architektúra</h1>
-            <h1>Architektonické navrhování budov </h1>
+            <h1>Architektonické navrhování budov 1-17</h1>
             <div>
                 {current_1 && (
                         <div style={{border: '1px solid', padding: "10px", fontSize: '20px'}}>
@@ -54,6 +64,15 @@ function App() {
                         </div>
                 )}
             <button onClick={() => randomNumbers(1)}>Generuj</button>
+            </div>
+            <h1>Architektonické navrhování budov 18-45</h1>
+            <div>
+                {current_4 && (
+                        <div style={{border: '1px solid', padding: "10px", fontSize: '20px'}}>
+                            <p>{current_4.question}</p>
+                        </div>
+                )}
+            <button onClick={() => randomNumbers(4)}>Generuj</button>
             </div>
             <h1>Konstrukcí pozemních staveb a stavební fyziky.  </h1>
             <div>
